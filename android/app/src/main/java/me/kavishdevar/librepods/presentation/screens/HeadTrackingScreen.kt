a811d38cad758ad8fb29946c01f69f0e35f49d79
tomppi <tomppi_94@hotmail.com>
2026-08-11 12:29:37 +0300

    Fix experimental Material3 opt-in on HeadTrackingScreen

--- android/app/src/main/java/me/kavishdevar/librepods/presentation/screens/HeadTrackingScreen.kt
+++ android/app/src/main/java/me/kavishdevar/librepods/presentation/screens/HeadTrackingScreen.kt
@@ -39,7 +39,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -57,7 +56,6 @@ import me.kavishdevar.librepods.presentation.viewmodel.AirPodsViewModel
 import me.kavishdevar.librepods.utils.HeadTracking
 import kotlin.io.encoding.ExperimentalEncodingApi
 
-@ExperimentalMaterial3Api
 @Composable
 fun HeadTrackingScreen(viewModel: AirPodsViewModel) {
     val state by viewModel.uiState.collectAsState()
